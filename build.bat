ae1cf5c67fdb10e8e787a4e8a61434005ab2ad59
on-keyday <62627905+on-keyday@users.noreply.github.com>
2023-08-12 23:20:25 +0900

    add CMakeLists.txt to src

--- build.bat
+++ build.bat
@@ -2,6 +2,6 @@
 setlocal
 set BUILD_TYPE=Debug
 set UTILS_DIR=C:/workspace/utils_backup
-cmake -D CMAKE_CXX_COMPILER=clang++ -D CMAKE_C_COMPILER=clang -G Ninja -D CMAKE_BUILD_TYPE=%BUILD_TYPE% -S . -B .
-ninja
+cmake -D CMAKE_CXX_COMPILER=clang++ -D CMAKE_C_COMPILER=clang -G Ninja -D CMAKE_BUILD_TYPE=%BUILD_TYPE% -S . -B ./build
+ninja -C ./build/
 
--- clean.bat
+++ clean.bat
@@ -1,10 +1,3 @@
 @echo off
 setlocal
-ninja clean
-rmdir /Q /S %CD%\CMakeFiles
-rmdir /Q /S %CD%\test
-del /Q %CD%\.ninja_deps
-del /Q %CD%\.ninja_log
-del /Q %CD%\CMakeCache.txt
-del /Q %CD%\cmake_install.cmake
-del /Q %CD%\build.ninja
+rmdir /Q /S %CD%\build
