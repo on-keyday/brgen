423d484827be15385b82a52f118113ab89fb9747
on-keyday <62627905+on-keyday@users.noreply.github.com>
2023-09-19 11:32:00 +0900

    start brgen code implementation

--- script/clone_utils.sh
+++ script/clone_utils.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/bash
 git clone https://github.com/on-keyday/utils --depth 1
 cd utils
-cmake  -D CMAKE_CXX_COMPILER=clang++ -D CMAKE_C_COMPILER=clang -G Ninja -D CMAKE_BUILD_TYPE=Debug -S . -B .
-ninja utils
+. build shared Debug utils
 cd ..
