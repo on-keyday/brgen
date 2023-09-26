cc422b2cbbcc06e1929426c164b1f689c0902a3b
on-keyday <62627905+on-keyday@users.noreply.github.com>
2023-09-26 21:02:54 +0900

    inline test text

--- src/core/lexer/lexer.h
+++ src/core/lexer/lexer.h
@@ -9,7 +9,6 @@
 #include <comb2/composite/string.h>
 #include "token.h"
 #include <optional>
-#include "lexer_test_text.h"
 
 namespace brgen::lexer {
 
@@ -69,6 +68,49 @@ namespace brgen::lexer {
         constexpr auto parse_one = one_token_lexer();
 
         constexpr auto check_lexer() {
+            constexpr auto test_text = R"a(
+format QUICPacket: 
+   form :b1
+   if form:
+      :LongPacket
+   else:
+      :OneRTTPacket
+   
+
+format LongPacket:
+   fixed :b1
+   long_packet_type :b2
+   reserved :b2
+   packet_number_length :b2
+   version :u32
+   
+
+format ConnectionID:
+   id :[]byte
+   fn encode():
+        pass      
+
+
+format Varint:
+   value :u64
+   fn decode(input):
+      p = input[0]
+      value = match p&0xC0 >> 6:
+         0 => input.u8() ~ msb(2)
+         1 => input.u16() ~ msb(2)
+         2 => input.u32() ~ msb(2)
+         3 => input.u64() ~ msb(2)
+
+   fn encode(output):
+      match value:
+         ..0x40 => output.u8(value.u8())
+         ..0x4000 => output.u16(value.u16() | msb(2,1))
+         ..0x40000000 => output.u32(value.u32() | msb(2,2))
+         ..0x4000000000000000 => output.u64(value | msb(2,3))
+         _ => error("too large number")
+
+)a";
+
             auto seq = utils::make_ref_seq(test_text);
             auto ctx = utils::comb2::test::TestContext<Tag>{};
             Tag m[] = {
