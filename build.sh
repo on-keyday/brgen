77132db5c4178bc5db037ccd9e0c6a201b1406c8
on-keyday <62627905+on-keyday@users.noreply.github.com>
2023-10-15 22:39:01 +0900

    why do I add web directory?

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ if [ ! -d utils ]; then
 . ./script/clone_utils.sh
 go mod download
 fi
+BUILD_MODE=native
 BUILD_TYPE=Debug
 UTILS_DIR=$(pwd)/utils cmake  -D CMAKE_CXX_COMPILER=clang++ -D CMAKE_C_COMPILER=clang -G Ninja -D CMAKE_BUILD_TYPE=$BUILD_TYPE -S . -B ./build
 ninja -C ./built/
