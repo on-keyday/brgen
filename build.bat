d4210e194e0784af1c1917255d43dc8365661dd3
on-keyday <62627905+on-keyday@users.noreply.github.com>
2023-10-15 23:53:57 +0900

    add install for build

--- build.bat
+++ build.bat
@@ -11,13 +11,15 @@ set BUILD_TYPE=Debug
 set UTILS_DIR=C:/workspace/utils_backup
 set LLVM_DIR=C:/workspace/llvm-project/llvm/build/lib/cmake/llvm
 set Clang_DIR=C:/workspace/llvm-project/clang/build/cmake/modules/CMakeFiles
+set INSTALL_PREFIX=.
 if "%BUILD_MODE%" == "native" (
-cmake -D CMAKE_CXX_COMPILER=clang++ -D CMAKE_C_COMPILER=clang -G Ninja -D CMAKE_BUILD_TYPE=%BUILD_TYPE% -S . -B ./built/%BUILD_MODE%/%BUILD_TYPE%
+cmake -D CMAKE_CXX_COMPILER=clang++ -D CMAKE_C_COMPILER=clang -G Ninja -DCMAKE_INSTALL_PREFIX=%INSTALL_PREFIX% -D CMAKE_BUILD_TYPE=%BUILD_TYPE% -S . -B ./built/%BUILD_MODE%/%BUILD_TYPE%
 ) else if "%BUILD_MODE%" == "wasm-em" (
-call emcmake cmake -G Ninja -D CMAKE_BUILD_TYPE=%BUILD_TYPE% -S . -B ./built/%BUILD_MODE%/%BUILD_TYPE%
+call emcmake cmake -G Ninja -D CMAKE_BUILD_TYPE=%BUILD_TYPE% -DCMAKE_INSTALL_PREFIX=%INSTALL_PREFIX%/web -S . -B ./built/%BUILD_MODE%/%BUILD_TYPE%
 ) else (
     echo "Invalid build mode: %BUILD_MODE%"
     exit 1
 )
 ninja -C ./built/%BUILD_MODE%/%BUILD_TYPE%
+ninja -C ./built/%BUILD_MODE%/%BUILD_TYPE% install
 
