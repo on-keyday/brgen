7dc17c6a0772607a429bc5ef59e3c2a15c2a04cf
on-keyday <62627905+on-keyday@users.noreply.github.com>
2023-10-17 14:16:06 +0900

    fix build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,18 @@ if [ ! -d utils ]; then
 . ./script/clone_utils.sh
 go mod download
 fi
-BUILD_MODE=native
+BUILD_MODE=$1
+
+if [ ! BUILD_MODE ]; then
+    BUILD_MODE=native
+fi
+
 BUILD_TYPE=Debug
-UTILS_DIR=$(pwd)/utils cmake  -D CMAKE_CXX_COMPILER=clang++ -D CMAKE_C_COMPILER=clang -G Ninja -D CMAKE_BUILD_TYPE=$BUILD_TYPE -S . -B ./build
-ninja -C ./built/
+UTILS_DIR=$(pwd)/utils
+if [ $BUILD_MODE = "wasm-em" ];then
+    emcmake cmake -G Ninja -D CMAKE_BUILD_TYPE=$BUILD_TYPE -S . -B ./built/$BUILD_MODE/$BUILD_TYPE
+else
+    cmake  -D CMAKE_CXX_COMPILER=clang++ -D CMAKE_C_COMPILER=clang -G Ninja -D CMAKE_BUILD_TYPE=$BUILD_TYPE -S . -B ./built/$BUILD_MODE/$BUILD_TYPE
+fi
+ninja -C ./built/$BUILD_MODE/$BUILD_TYPE
+ninja -C ./built/$BUILD_MODE/$BUILD_TYPE install
