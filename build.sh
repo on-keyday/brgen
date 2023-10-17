9485251dbaf41c37809e690851e69c6fb3769063
on-keyday <62627905+on-keyday@users.noreply.github.com>
2023-10-17 14:18:25 +0900

    fix build.sh

--- build.sh
+++ build.sh
@@ -10,11 +10,11 @@ if [ ! BUILD_MODE ]; then
 fi
 
 BUILD_TYPE=Debug
-UTILS_DIR=$(pwd)/utils
+
 if [ $BUILD_MODE = "wasm-em" ];then
-    emcmake cmake -G Ninja -D CMAKE_BUILD_TYPE=$BUILD_TYPE -S . -B ./built/$BUILD_MODE/$BUILD_TYPE
+   UTILS_DIR=$(pwd)/utils emcmake cmake -G Ninja -D CMAKE_BUILD_TYPE=$BUILD_TYPE -S . -B ./built/$BUILD_MODE/$BUILD_TYPE
 else
-    cmake  -D CMAKE_CXX_COMPILER=clang++ -D CMAKE_C_COMPILER=clang -G Ninja -D CMAKE_BUILD_TYPE=$BUILD_TYPE -S . -B ./built/$BUILD_MODE/$BUILD_TYPE
+   UTILS_DIR=$(pwd)/utils cmake  -D CMAKE_CXX_COMPILER=clang++ -D CMAKE_C_COMPILER=clang -G Ninja -D CMAKE_BUILD_TYPE=$BUILD_TYPE -S . -B ./built/$BUILD_MODE/$BUILD_TYPE
 fi
 ninja -C ./built/$BUILD_MODE/$BUILD_TYPE
 ninja -C ./built/$BUILD_MODE/$BUILD_TYPE install
