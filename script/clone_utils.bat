650722ae1b289aa4139bd393679214531d2f4973
on-keyday <62627905+on-keyday@users.noreply.github.com>
2024-01-03 19:30:41 +0900

    add network auto build

--- script/clone_utils.bat
+++ script/clone_utils.bat
@@ -11,5 +11,9 @@ if "%1" == "wasm-em" (
     call build wasm-em %BUILD_TYPE% futils
 ) else (
     call build shared %BUILD_TYPE% futils
+    if "%S2J_USE_NETWORK%" == "1" (
+        call build shared %BUILD_TYPE% fnet
+        call build shared %BUILD_TYPE% fnetserv
+    )
 )
 cd ..
