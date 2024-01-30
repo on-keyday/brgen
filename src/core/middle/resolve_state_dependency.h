7165e317658ed25d767de59f67dd0622ae02b913
on-keyday <62627905+on-keyday@users.noreply.github.com>
2024-01-30 17:46:07 +0900

    add state variable tracking

--- src/core/middle/resolve_state_dependency.h
+++ src/core/middle/resolve_state_dependency.h
@@ -10,15 +10,20 @@ namespace brgen::middle {
         std::set<ast::Format*> fmts;
         auto traverse_fn = [&](auto&& f, const std::shared_ptr<ast::Node>& n) -> void {
             if (auto fmt = ast::as<ast::Format>(n)) {
+                if (fmts.find(fmt) != fmts.end()) {
+                    return;  // already visited
+                }
+                fmts.insert(fmt);
                 for (auto& d : fmt->depends) {
                     auto p = d.lock();
                     if (p) {
-                        auto b = ast::as<ast::Format>(p->ident->base.lock());
-                        if (b) {
+                        auto l = p->ident->base.lock();
+                        if (auto b = ast::as<ast::Format>(l)) {
+                            f(f, l);
                             fmt->state_variables.insert(fmt->state_variables.end(), b->state_variables.begin(), b->state_variables.end());
                         }
                     }
-                    auto kept = std::unique(fmt->state_variables.begin(), fmt->state_variables.end(),[](auto&& a,auto&& b){
+                    auto kept = std::unique(fmt->state_variables.begin(), fmt->state_variables.end(), [](auto&& a, auto&& b) {
                         return a.lock() == b.lock();
                     });
                     fmt->state_variables.erase(kept, fmt->state_variables.end());
