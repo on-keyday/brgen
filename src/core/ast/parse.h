2fa30f9fbae0db5ef6ee48914118af5f4ff867f7
on-keyday <62627905+on-keyday@users.noreply.github.com>
2024-02-26 02:14:55 +0900

    remove fn cast syntax

--- src/core/ast/parse.h
+++ src/core/ast/parse.h
@@ -1397,11 +1397,13 @@ namespace brgen::ast {
         std::shared_ptr<Function> parse_fn(lexer::Token&& token) {
             auto fn = std::make_shared<Function>(token.loc);
             s.skip_white();
+            /*
             if (auto cast = s.consume_token("cast")) {
                 fn->is_cast = true;
                 fn->cast_loc = cast->loc;
                 s.skip_white();
             }
+            */
             fn->ident = parse_ident();
             fn->ident->usage = fn->is_cast ? IdentUsage::define_cast_fn : IdentUsage::define_fn;
             fn->ident->base = fn;
