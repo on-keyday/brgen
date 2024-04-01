68e65d81e37a39e5ae9bec51991842fc5d71f0cc
on-keyday <62627905+on-keyday@users.noreply.github.com>
2024-04-01 22:19:30 +0900

    Update README and setup.md files, fix view of depends

--- ast2ts/src/analyze.ts
+++ ast2ts/src/analyze.ts
@@ -193,7 +193,7 @@ export const analyzeHover =  (prevNode :ast2ts.Node, pos :number) =>{
     + fixed tail size: ${bitSize(ident.base.body?.struct_type?.fixed_tail_size)}
     + algin: ${ident.base.body?.struct_type?.bit_alignment||"unknown"}
     ${ident.base.body?.struct_type?.non_dynamic_allocation?"+ non_dynamic\n    ":""}${ident.base.body?.struct_type?.recursive?"+ recursive\n":""}
-    ${ident.base.depends.length > 0 ?`+ depends: ${ident.base.depends.map((x)=>x.ident?.ident||"(null)").join(", ")}\n`:""}
+    ${ident.base.depends.length > 0 ?`+ depends: ${ident.base.depends.map((x)=>x.ident?.ident||"(null)").filter((elem, index, self) => self.indexOf(elem) === index).join(", ")}\n`:""}
     ${ident.base.state_variables.length > 0 ?`+ state variables: ${ident.base.state_variables.map((x)=>x.ident?.ident||"(null)").join(", ")}\n`:""}
     ${ident.base.encode_fn?"+ custom encode\n":""}
     ${ident.base.decode_fn?"+ custom decode\n":""}
