e8e57c75e801d3ae15e7d46a355d2baf28b777a9
on-keyday <62627905+on-keyday@users.noreply.github.com>
2024-04-23 14:21:59 +0900

    add mutex for sync write

--- src/tool/common/send.h
+++ src/tool/common/send.h
@@ -3,22 +3,35 @@
 #include "print.h"
 #include <request/stream.hpp>
 #include <core/common/util.h>
-#if __has_include(<thread>)
+#if __has_include(<thread>) && __has_include(<mutex>)
 #include <thread>
 #include <thread/channel.h>
+#include <mutex>
 #define HAS_THREAD
 #endif
 
-inline void send_response(brgen::request::SourceCode&& code) {
+inline void do_send(auto&& cb) {
+#ifdef HAS_THREAD
+    static std::mutex send_mutex;
+    futils::file::FileStream<std::string, const futils::file::File&, std::mutex&> fs{cout.get_file(), send_mutex};
+#else
     futils::file::FileStream<std::string> fs{cout.get_file()};
+#endif
+
     futils::binary::writer w{fs.get_write_handler(), &fs};
     auto prev = w.offset();
-    if (auto err = brgen::request::write_response(w, std::move(code))) {
+    if (auto err = cb(w)) {
         assert(false);  // should not happen
         w.reset(prev);  // reset for consistency
         return;         // ignore error
     }
     // commit by writer handler
+}
+
+inline void send_response(brgen::request::SourceCode&& code) {
+    return do_send([&](futils::binary::writer& w) -> futils::error::Error<> {
+        return brgen::request::write_response(w, std::move(code));
+    });
 }
 
 thread_local bool send_as_text = false;
@@ -27,16 +40,9 @@ inline void send_end_response(std::uint64_t id) {
     if (send_as_text) {
         return;
     }
-    brgen::request::EndOfCode eoc;
-    eoc.id = id;
-    futils::file::FileStream<std::string> fs{cout.get_file()};
-    futils::binary::writer w{fs.get_write_handler(), &fs};
-    if (auto err = brgen::request::write_end_response(w, id)) {
-        assert(false);  // should not happen
-        w.reset(0);     // reset for consistency
-        return;         // ignore error
-    }
-    // commit by writer handler
+    return do_send([&](futils::binary::writer& w) -> futils::error::Error<> {
+        return brgen::request::write_end_response(w, id);
+    });
 }
 
 inline void send_error(std::uint64_t id, auto&&... msg) {
@@ -93,7 +99,7 @@ inline void read_stdin_requests_impl(auto&& cb) {
     if (send_as_text) {
         return;
     }
-    send_response_header();
+    send_response_header();  // here not multi-thread
     if (auto err = brgen::request::read_requests(futils::file::File::stdin_file(), cb)) {
         send_error_and_end(0, "cannot read requests: ", err.template error<std::string>());
     }
