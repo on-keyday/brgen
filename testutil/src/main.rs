1bc05781ba6f8e39817951dbe362c613260bd00c
on-keyday <62627905+on-keyday@users.noreply.github.com>
2024-04-24 21:40:56 +0900

    adding schema

--- testutil/src/main.rs
+++ testutil/src/main.rs
@@ -1,11 +1,24 @@
 use clap::{arg, Parser};
+use serde::{Deserialize, Serialize};
 
 #[derive(Parser)]
 struct Args {
-    #[arg(long, short)]
-    spec: bool,
+    #[arg(long, short('f'))]
+    test_info_file: String,
 }
 
+#[derive(Serialize, Deserialize)]
+struct GeneratedFile {
+    dir: String,
+    base: String,
+    suffix: String,
+}
 
+struct TestInfo {
+    total_count: u64,
+    err_count: u64,
+    time: String,
+    generated_files: GeneratedFile,
+}
 
 fn main() {}
