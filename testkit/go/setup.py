94fa91af96804d488419a9408563b7eb9ac7078e
on-keyday <62627905+on-keyday@users.noreply.github.com>
2024-05-09 15:23:07 +0900

    run go mod for test

--- testkit/go/setup.py
+++ testkit/go/setup.py
@@ -7,14 +7,20 @@ INPUT = sys.argv[1]
 OUTPUT = sys.argv[2]
 ORIGIN = sys.argv[3]
 TMPDIR = sys.argv[4]
+DEBUG = True if sys.argv[5] == "true" else False
 
 # replace `package xxx\n` (xxx is any string) with `package main\n`
-COMPILED = re.compile("package (.*)\n")
+COMPILED = re.compile("package .*\n")
 
 with open(INPUT, "r") as fp:
     TEXT = fp.read()
-    TEXT = COMPILED.sub("main", TEXT)
-
+    if DEBUG:
+        print(INPUT, "before replace")
+        print(TEXT)
+    TEXT = COMPILED.sub("package main", TEXT)
+    if DEBUG:
+        print(INPUT, "after replace")
+        print(TEXT)
 
 with open(INPUT, "w") as fp:
     fp.write(TEXT)
