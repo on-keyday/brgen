8c8738cc870d84c43c212559617dee974ab7ea08
on-keyday <62627905+on-keyday@users.noreply.github.com>
2024-06-20 20:01:31 +0900

    add cache for web

--- web/dev/test/puppet.mjs
+++ web/dev/test/puppet.mjs
@@ -95,7 +95,7 @@ console.log("PWD:", PWD);
 if(!PWD.endsWith("/")) {
     PWD += "/";
 }
-const examples = await readdir(PWD + "../../../example",{recursive:true}).then(x => x.map((e) => e.replace("\\","/")));
+const examples = await readdir(PWD + "../../example",{recursive:true}).then(x => x.map((e) => e.replace("\\","/")));
 console.log("examples:", examples);
 
 const writeCommandPalette = async (fileName) => {
