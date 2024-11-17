9870284fde3717179f72afeb8f63b6a33dbf2d8c
on-keyday <62627905+on-keyday@users.noreply.github.com>
2024-11-17 20:27:50 +0900

    remove

--- web/dev/webpack.config.js
+++ web/dev/webpack.config.js
@@ -1,18 +1,21 @@
 const { copyFile } = require("fs");
-const process = require("process")
+const process = require("process");
 const path = require("path");
 
-
 const copyWasm = (p) => {
-    copyFile(path.resolve(__dirname, "src/lib/",p), path.resolve(__dirname, "out/lib/",p), (err) => {
-        if (err) {
-            console.log(err);
-        }
-    });
-}
+  copyFile(
+    path.resolve(__dirname, "src/lib/", p),
+    path.resolve(__dirname, "out/lib/", p),
+    (err) => {
+      if (err) {
+        console.log(err);
+      }
+    }
+  );
+};
 
 copyWasm("src2json.wasm");
-copyWasm("json2cpp.wasm");
+//copyWasm("json2cpp.wasm");
 copyWasm("json2cpp2.wasm");
 copyWasm("json2go.wasm");
 copyWasm("json2c.wasm");
@@ -21,37 +24,37 @@ copyWasm("json2kaitai.wasm");
 
 let mode = "development";
 if (process.env.WEB_PRODUCTION === "production") {
-    mode = "production";
+  mode = "production";
 }
 
 module.exports = {
-    mode: mode,
-    entry: path.resolve(__dirname, "out/index.js"),
-    resolve: {
-        extensions: [".ts",".tsx",".js"],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(ts|tsx)$/,
-                loader: "ts-loader",
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"],
-            },
-        ],
-    },
-    output: {
-        filename: "index.js",
-        path: path.resolve(__dirname, "../public/script"),
-        assetModuleFilename: "[name][ext]",
-    }, 
-    stats: {
-        errorDetails: true,
-    },
-}
+  mode: mode,
+  entry: path.resolve(__dirname, "out/index.js"),
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        loader: "ts-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "../public/script"),
+    assetModuleFilename: "[name][ext]",
+  },
+  stats: {
+    errorDetails: true,
+  },
+};
 
 if (mode === "development") {
-    module.exports.devtool = "hidden-source-map"
+  module.exports.devtool = "hidden-source-map";
 }
