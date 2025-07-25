c1e0d1bac7cfd9f65e436c4e99b3318495dd192f
on-keyday <62627905+on-keyday@users.noreply.github.com>
2025-07-25 19:18:57 +0900

    current change

--- web/dev/tree-sitter-brgen/grammar.js
+++ web/dev/tree-sitter-brgen/grammar.js
@@ -3,27 +3,66 @@ module.exports = grammar({
 
   extras: ($) => [],
 
+  conflicts: ($) => [[$.expr]],
+
   rules: {
     program: ($) =>
       seq(
-        optional($.skip_lines),
-        repeat(seq($._statement, optional($.skip_white)))
+        optional($._skip_lines),
+        repeat(seq($._statement, optional($._skip_white)))
       ),
-    skip_lines: ($) => repeat1(choice($.space, $.comment, $.line)),
-    skip_white: ($) => repeat1(choice($.space, $.comment, $.line)),
-    skip_space: ($) => repeat1(choice($.space, $.comment)),
+    _skip_lines: ($) => repeat1(choice($._space, $._comment, $._line)),
+    _skip_white: ($) => repeat1(choice($._space, $._comment, $._line)),
+    _skip_space: ($) => repeat1(choice($._space, $._comment)),
 
     _statement: ($) => choice($.expr),
 
-    space: ($) => /[ \t]+/,
-    line: ($) => /[\r\n]+/,
-    comment: ($) => /#.*[\r\n]*/,
+    _space: ($) => /[ \t]+/,
+    _line: ($) => /[\r\n]+/,
+    _comment: ($) => /#.*[\r\n]*/,
 
-    expr: ($) => choice($._unary),
+    identifier: ($) => /[a-zA-Z_][a-zA-Z0-9_]*/,
+
+    mul_op: ($) => choice("*", "/", "%", "&", "<<<", ">>>", "<<", ">>"),
+    add_op: ($) => choice("+", "-", "|", "^"),
+    cmp_op: ($) => choice("==", "!=", "<", ">", "<=", ">="),
+    logical_and_op: ($) => "&&",
+    logical_or_op: ($) => "||",
+
+    expr: ($) =>
+      choice(
+        prec.left(
+          7,
+          seq($.expr, $._skip_space, $.logical_or_op, $._skip_white, $.expr)
+        ),
+        prec.left(
+          8,
+          seq($.expr, $._skip_space, $.logical_and_op, $._skip_white, $.expr)
+        ),
+        prec.left(
+          9,
+          seq($.expr, $._skip_space, $.cmp_op, $._skip_white, $.expr)
+        ),
+        prec.left(
+          10,
+          seq($.expr, $._skip_space, $.add_op, $._skip_white, $.expr)
+        ),
+        prec.left(
+          20,
+          seq($.expr, $._skip_space, $.mul_op, $._skip_white, $.expr)
+        ),
+        prec.left(30, $._unary)
+      ),
 
     unary_op: ($) => /[-!]/,
 
-    _unary: ($) => choice(seq($.unary_op, $._unary), $.prim),
+    _unary: ($) => choice(seq($.unary_op, $._unary), $.postfix),
+
+    postfix: ($) => seq($.prim, repeat(choice($.member_access, $.call))),
+
+    member_access: ($) => seq(".", $.identifier),
+    call: ($) => seq("(", optional($.args), ")"),
+    args: ($) => seq($.expr, repeat(seq(",", $.expr))),
 
     prim: ($) => choice($.int_literal),
 
